124048552df2d414e25e7627b556a73c0f33fc30
Orpheon <alex.liebendorf@gmail.com>
2020-11-22 15:06:16 +0100

    Added a random trip construction method.

--- src/main/java/ch/mse/santachallenge/Program.java
+++ src/main/java/ch/mse/santachallenge/Program.java
@@ -4,6 +4,7 @@ import ch.mse.santachallenge.utils.CsvReader;
 import ch.mse.santachallenge.utils.Printer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
@@ -17,5 +18,24 @@ public class Program {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<Trip> constructRandomSolution(List<Gift> gifts) {
+        ArrayList<Trip> trips = new ArrayList<Trip>(100);
+        int id_template = 0;
+        // Add first trip with id 0
+        trips.add(new Trip(id_template++));
+        for (Gift gift : gifts) {
+            // Add each gift to a random trip. If the trip is already full, create a new trip and add the gift there
+            Trip trip = trips.get((int) (Math.random() * trips.size()));
+            if (trip.totalWeight() + gift.getWeight() <= Constants.maxWeight) {
+                trip.add(gift);
+            } else {
+                Trip newTrip = new Trip(id_template++);
+                newTrip.add(gift);
+                trips.add(newTrip);
+            }
+        }
+        return trips;
     }
 }
