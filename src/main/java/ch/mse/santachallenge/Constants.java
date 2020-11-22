c99ce7cafc27d1ee1fab11db0506fcbe6952e2c6
Orpheon <alex.liebendorf@gmail.com>
2020-11-22 14:42:14 +0100

    Added constants file with the constants from the challenge.

---
+++ src/main/java/ch/mse/santachallenge/Constants.java
@@ -0,0 +1,7 @@
+package ch.mse.santachallenge;
+
+public class Constants {
+    public static final Location northPole = new Location(0, 0);
+    public static final double sledWeight = 10.0;
+    public static final double maxWeight = 1000.0 + sledWeight;
+}
